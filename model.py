6b7410ab2e1038a0a7ee584558d5e6edb8e580c9
ericamcg <71267518+ericamcg@users.noreply.github.com>
2020-09-17 13:30:20 +0100

    Extra exercises functions

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
 import random
 import operator
 import matplotlib.pyplot
+from itertools import combinations
+import time
 
+# Function to find the distance between 2 agents 
 def distance_between(agents_row_a, agents_row_b):
     diff_y = (agents_row_a[0] - agents_row_b[0])
     diff_x = (agents_row_a[1] - agents_row_b[1])
@@ -31,15 +34,6 @@ for _ in range(num_of_it):
             agents[i][1] = (agents[i][1] - 1) % 100
 print(agents)
 
-# Answer = pythaian distance between agents
-distance_between(agents[0], agents[1])
-
-# for loop for every agent 
-for first_agent in agents:
-    for second_agent in agents:
-        distance = distance_between(first_agent, second_agent)
-        print(first_agent, second_agent, distance)
-
 # Plot agent locations 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
@@ -48,3 +42,37 @@ for i in range(num_of_agents):
 # m = max(agents, key=operator.itemgetter(1))
 # matplotlib.pyplot.scatter(m[1],m[0], color='red')
 matplotlib.pyplot.show()
+
+# Distance between any 2 agents (any number between 0 and 9 can be given)
+distance_between_2 = distance_between(agents[0], agents[1])
+print(distance_between_2)
+
+start = time.process_time()
+
+# Creating an empty list for the distances to append to
+# We can append distance values to this in the for loop, to later find the max and min
+distance_list = []
+
+# for loop to find the distance between every agent combination
+# This loop uses combinations, a function which returns tuples with no repeated combinations 
+# This removes pairs repeating and distances between the same agent 
+
+for first_agent, second_agent in combinations(agents, 2):
+    distance = distance_between(first_agent, second_agent)
+    distance_list.append(distance)
+print(distance_list)
+
+end = time.process_time()
+print(end - start)
+
+# Finding the minimum and maximum distances 
+print(distance_list)
+max(distance_list)
+min(distance_list)
+
+# Graph for times 
+agent_numbers = [10, 100, 500, 1000, 2000, 3000]
+times = [0.0005, 0.0088, 0.1924, 0.7024, 2.7459, 6.1976]
+matplotlib.pyplot.plot(agent_numbers, times, marker = 'o', color = "r")
+matplotlib.pyplot.xlabel('Number of agents')
+matplotlib.pyplot.ylabel('Time (s)')
