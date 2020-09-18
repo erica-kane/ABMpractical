9877113f1fc9d85fdc4bd1e9e92e1fbab934a9f0
ericamcg <71267518+ericamcg@users.noreply.github.com>
2020-09-18 16:50:03 +0100

    I/O practical

--- agentframework.py
+++ agentframework.py
@@ -1,17 +1,38 @@
 import random
 
 class Agent():
-    def __init__(self):
-        self.y = random.randint(0, 99)
-        self.x = random.randint(0, 99)
+    def __init__(self, environment):
+        self.y_len = len(environment)
+        self.x_len = len(environment[0])
+        self.y = random.randint(0, self.y_len)
+        self.x = random.randint(0, self.x_len)
+        self.environment = environment
+        self.store = 0 
 
     def move (self):
         if random.random() < 0.5:
-            self.y = (self.y + 1) % 100
+            self.y = (self.y + 1) % self.y_len
         else:
-            self.y = (self.y - 1) % 100
+            self.y = (self.y - 1) % self.y_len
 
         if random.random() < 0.5:
-            self.x = (self.x + 1) % 100
+            self.x = (self.x + 1) % self.x_len
         else:
-            self.x = (self.x - 1) % 100
+            self.x = (self.x - 1) % self.x_len
+
+    def eat(self):
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 10
+        else:
+            self.store += self.environment[self.y][self.x]
+            self.environment[self.y][self.x] -= self.environment[self.y][self.x]
+
+    def sick(self):
+        if self.store > 100:
+            self.environment[self.y][self.x] += self.store
+            self.store = 0 
+
+    def __str__(self):
+        return 'Agent: ' + 'x = ' + str(self.x) + ', y = ' + str(self.y) + ', store = ' + str(self.store)
+
