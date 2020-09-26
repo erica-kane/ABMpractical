21524f029264e72e9521ee2b780d30563b813158
ericamcg <71267518+ericamcg@users.noreply.github.com>
2020-09-26 10:58:44 +0100

    fixing coordinates

--- agentframework.py
+++ agentframework.py
@@ -1,24 +1,25 @@
 import random
 
 class Agent():
-    def __init__(self, environment):
-        self.y_len = len(environment)
-        self.x_len = len(environment[0])
-        self.y = random.randint(0, self.y_len)
-        self.x = random.randint(0, self.x_len)
+    def __init__(self, environment, agents):
+        self.y_max = len(environment) - 1
+        self.x_max = len(environment[0]) - 1
+        self.y = random.randint(0, self.y_max)
+        self.x = random.randint(0, self.x_max)
         self.environment = environment
         self.store = 0 
+        self.agents = agents
 
     def move (self):
         if random.random() < 0.5:
-            self.y = (self.y + 1) % self.y_len
+            self.y = (self.y + 1) % self.y_max
         else:
-            self.y = (self.y - 1) % self.y_len
+            self.y = (self.y - 1) % self.y_max 
 
         if random.random() < 0.5:
-            self.x = (self.x + 1) % self.x_len
+            self.x = (self.x + 1) % self.x_max
         else:
-            self.x = (self.x - 1) % self.x_len
+            self.x = (self.x - 1) % self.x_max
 
     def eat(self):
         if self.environment[self.y][self.x] > 10:
@@ -27,11 +28,24 @@ class Agent():
         else:
             self.store += self.environment[self.y][self.x]
             self.environment[self.y][self.x] -= self.environment[self.y][self.x]
-
-    def sick(self):
+        
         if self.store > 100:
             self.environment[self.y][self.x] += self.store
             self.store = 0 
+
+    def share(self, neighbourhood):
+        for agent in self.agents:
+            if agent != self:
+                distance = self.distance_between(agent)
+                if distance <= neighbourhood:
+                    sum = self.store + agent.store
+                    average = sum/2
+                    self.store = average 
+                    agent.store = average 
+                    print("sharing " + str(distance) + " " + str(average))
+    
+    def distance_between(self, agent):
+        return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
 
     def __str__(self):
         return 'Agent: ' + 'x = ' + str(self.x) + ', y = ' + str(self.y) + ', store = ' + str(self.store)
