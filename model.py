152c8d507869037615316a24fc510a04884298a4
ericamcg <71267518+ericamcg@users.noreply.github.com>
2020-09-26 10:58:25 +0100

    Animation update

--- model.py
+++ model.py
@@ -1,10 +1,14 @@
 import random
 import operator
 import matplotlib.pyplot
+import matplotlib.animation 
 from itertools import combinations
 import time
 import csv
 import agentframework
+
+# %matplotlib qt
+# %matplotlib inline
 
 # Read the CSV
 environment = []
@@ -27,55 +31,62 @@ f.close()
 y_len = len(environment)
 x_len = len(environment[0])
 
-# Make an single agent object
-a = agentframework.Agent(environment)
-print(a)
-print(a.x, a.y)
-a.move()
-print(a.x, a.y)
-
-# Function to find the distance between 2 agents 
-def distance_between(agent_a, agent_b):
-    diff_y = (agent_a.x - agent_b.x)
-    diff_x = (agent_a.y - agent_b.y)
-    distance = (diff_y**2 + diff_x**2)**0.5
-    return distance
-
 # Creating a list for agents and variables
 agents = []
-num_of_agents = 10 
+num_of_agents = 10
 num_of_it = 100
+neighbourhood = 20
+
+# initialise the graph
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+
+# ax.set_autoscale_on(False)
+
+# Make an single agent object
+# a = agentframework.Agent(environment, agents)
+# print(a)
+# print(a.x, a.y)
+# a.move()
+# print(a.x, a.y)
 
 # Make the agents 
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent(environment))
-print(agents)
+    agents.append(agentframework.Agent(environment, agents))
 
-# Drawning an initial plot to compare 
-matplotlib.pyplot.ylim(0, y_len)
-matplotlib.pyplot.xlim(0, x_len)
-matplotlib.pyplot.imshow(environment)
-for agent in agents:
-    matplotlib.pyplot.scatter(agent.x, agent.y)
-# m = max(agents, key=operator.itemgetter(1))
-# matplotlib.pyplot.scatter(m[1],m[0], color='red')
-matplotlib.pyplot.show()
+# Prove the agents have access to the list of agents 
+agents[1].x
+agents[5].agents[1].x
+
+# # Drawning an initial plot to compare 
+# matplotlib.pyplot.ylim(0, y_len)
+# matplotlib.pyplot.xlim(0, x_len)
+# matplotlib.pyplot.imshow(environment)
+# for agent in agents:
+#     matplotlib.pyplot.scatter(agent.x, agent.y)
+# matplotlib.pyplot.show()
 
 # Move the agents 
-for _ in range(num_of_it):
+
+def update(frame_number):
+    
+    fig.clear()  
+
+    matplotlib.pyplot.imshow(environment)
+
+    #for _ in range(num_of_it):
+    #random.shuffle(agents)
     for agent in agents:
         agent.move()
         agent.eat()
-        agent.sick()
+        agent.share(neighbourhood)
 
-# Plot agent locations 
-matplotlib.pyplot.ylim(0, y_len)
-matplotlib.pyplot.xlim(0, x_len)
-matplotlib.pyplot.imshow(environment)
-for agent in agents:
-    matplotlib.pyplot.scatter(agent.x, agent.y)
-# m = max(agents, key=operator.itemgetter(1))
-# matplotlib.pyplot.scatter(m[1],m[0], color='red')
+    for agent in agents:
+        matplotlib.pyplot.scatter(agent.x, agent.y)
+  
+
+animation = matplotlib.animation.FuncAnimation(fig, update, interval = 1, repeat = False, frames = num_of_it)
+
 matplotlib.pyplot.show()
 
 # Writing out environment as a csv file 
@@ -93,36 +104,36 @@ storefile.write('Total agent store: ' + str(totalstore) + '\n')
 storefile.close()
 
 
-# Distance between any 2 agents (any number between 0 and 9 can be given)
-distance_between_2 = distance_between(agents[0], agents[1])
-print(distance_between_2)
+# Distance between any 2 given agents
+agents[0].distance_between(agents[6])
 
-# Creating an empty list for the distances to append to
-# We can append distance values to this in the for loop, to later find the max and min
-distance_list = []
-
-start = time.process_time()
+# start = time.process_time()
 
 # for loop to find the distance between every agent combination
 # This loop uses combinations, a function which returns tuples with no repeated combinations 
 # This removes pairs repeating and distances between the same agent 
 
-for first_agent, second_agent in combinations(agents, 2):
-    distance = distance_between(first_agent, second_agent)
-    distance_list.append(distance)
-print(distance_list)
+distance_list = []
 
-end = time.process_time()
-print(end - start)
+for first_agent, second_agent in combinations(agents, 2):
+    distance = first_agent.distance_between(second_agent)
+    distance_list.append(distance)
+
+for dist in distance_list:
+    if dist <= neighbourhood:
+        print(dist)
+
+# end = time.process_time()
+# print(end - start)
 
 # Finding the minimum and maximum distances 
-print(distance_list)
-max(distance_list)
-min(distance_list)
+# print(distance_list)
+# max(distance_list)
+# min(distance_list)
 
 # Graph for times when agents increase 
-agent_numbers = [10, 100, 500, 1000, 2000, 3000]
-times = [0.0005, 0.0088, 0.1924, 0.7024, 2.7459, 6.1976]
-matplotlib.pyplot.plot(agent_numbers, times, marker = 'o', color = "r")
-matplotlib.pyplot.xlabel('Number of agents')
-matplotlib.pyplot.ylabel('Time (s)')
+# agent_numbers = [10, 100, 500, 1000, 2000, 3000]
+# times = [0.0005, 0.0088, 0.1924, 0.7024, 2.7459, 6.1976]
+# matplotlib.pyplot.plot(agent_numbers, times, marker = 'o', color = "r")
+# matplotlib.pyplot.xlabel('Number of agents')
+# matplotlib.pyplot.ylabel('Time (s)')
